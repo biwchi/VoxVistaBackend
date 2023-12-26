c86fe136e3f889c23a37840ace7b1da2a51411f8
hodame <hodame04@gmail.com>
2023-12-26 21:33:42 +0500

    first commit

---
+++ src/main/java/com/biwhci/vistaback/VistaBackApplication.java
@@ -0,0 +1,13 @@
+package com.biwhci.vistaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VistaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VistaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biwhci/vistaback/VoxvistajavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biwhci.vistaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoxvistajavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
