664232496040933b5fbdf2aa6d6f4c73b5e18bcf
hodame <hodame04@gmail.com>
2024-01-10 18:22:40 +0500

    add validation when user voting for option

--- src/main/java/com/biwhci/vistaback/poll/services/PollOptionService.java
+++ src/main/java/com/biwhci/vistaback/poll/services/PollOptionService.java
@@ -10,7 +10,6 @@ import com.biwhci.vistaback.user.models.AppUser;
 import com.biwhci.vistaback.user.services.AppUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -24,13 +23,12 @@ public class PollOptionService {
   private final PollOptionRepository pollOptionRepository;
   private final PollRepository pollRepository;
   private final AppUserService appUserService;
+  private final PollService pollService;
 
   public void voteForOption(PollOptionVoteDto pollOptionVoteDto) {
-    AppUserDto principal = (AppUserDto) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    Optional<AppUser> user = appUserService.findByEmail(principal.getEmail());
-
+    Optional<AppUser> user = appUserService.getCurrentUser();
     Poll poll = pollRepository.findById(pollOptionVoteDto.getPollId()).orElseThrow(
-        () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, pollOptionVoteDto.getPollId() + "Such poll does not exist")
+        () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, pollOptionVoteDto.getPollId() + " Such poll does not exist")
     );
 
     if (user.isEmpty()) {
@@ -38,7 +36,7 @@ public class PollOptionService {
     }
 
     if (!poll.getMultiple() && pollOptionVoteDto.getOptionsIds().size() > 1) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You can only vote for one option in multiple choice poll");
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You can only vote for one option in none multiple choice poll");
     }
 
     if (poll.getStartDate() != null && poll.getStartDate().isAfter(ZonedDateTime.now())) {
@@ -59,13 +57,17 @@ public class PollOptionService {
           return pollOption.get();
         }).toList();
 
-    pollOptions.forEach(pollOption -> {
-      if (pollOption.getVoters().contains(user.get())) {
-        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You have already voted for this option");
-      }
+    if (pollService.isUserVoted(poll)) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You have already voted for this poll");
+    }
 
+    pollOptions.forEach(pollOption -> {
       pollOption.getVoters().add(user.get());
       pollOptionRepository.save(pollOption);
     });
+  }
+
+  public List<AppUserDto> getVoters(Integer pollOptionId) {
+    return appUserService.findUsersByVotedPollOptionId(pollOptionId);
   }
 }
