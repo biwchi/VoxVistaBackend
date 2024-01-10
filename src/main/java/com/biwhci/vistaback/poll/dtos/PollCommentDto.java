1a59ca3c8cab361a7a67464664a19274438d027f
hodame <hodame04@gmail.com>
2024-01-10 18:20:23 +0500

    add json format

--- src/main/java/com/biwhci/vistaback/poll/dtos/PollCommentDto.java
+++ src/main/java/com/biwhci/vistaback/poll/dtos/PollCommentDto.java
@@ -1,6 +1,7 @@
 package com.biwhci.vistaback.poll.dtos;
 
 import com.biwhci.vistaback.user.dtos.AppUserDto;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -11,6 +12,9 @@ import java.time.ZonedDateTime;
 public class PollCommentDto {
   private Integer id;
   private String content;
+
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
   private ZonedDateTime createdAt;
+
   private AppUserDto createdBy;
 }
