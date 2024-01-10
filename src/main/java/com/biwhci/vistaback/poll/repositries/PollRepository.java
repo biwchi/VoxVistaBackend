3add1e634e5da42383bc8b7a95bfea62b9398c54
hodame <hodame04@gmail.com>
2024-01-10 18:22:59 +0500

    add find by created by user id method in poll repo

--- src/main/java/com/biwhci/vistaback/poll/repositries/PollRepository.java
+++ src/main/java/com/biwhci/vistaback/poll/repositries/PollRepository.java
@@ -4,6 +4,9 @@ import com.biwhci.vistaback.poll.models.Poll;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PollRepository extends JpaRepository<Poll, Integer> {
+  List<Poll> findByCreatedByUserId(Integer userId);
 }
