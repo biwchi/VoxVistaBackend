194f1d935aab276c7ae689bb8256b9725ae5f194
hodame <hodame04@gmail.com>
2024-01-10 18:21:25 +0500

    add new field for mapper

--- src/main/java/com/biwhci/vistaback/poll/mappers/PollMapper.java
+++ src/main/java/com/biwhci/vistaback/poll/mappers/PollMapper.java
@@ -1,47 +1,45 @@
 package com.biwhci.vistaback.poll.mappers;
 
+import com.biwhci.vistaback.exceptions.UnauthorizedException;
 import com.biwhci.vistaback.poll.dtos.PollCreateDto;
 import com.biwhci.vistaback.poll.dtos.PollDto;
 import com.biwhci.vistaback.poll.models.Poll;
 import com.biwhci.vistaback.poll.models.PollOption;
-import com.biwhci.vistaback.user.dtos.AppUserDto;
+import com.biwhci.vistaback.poll.services.PollService;
 import com.biwhci.vistaback.user.models.AppUser;
 import com.biwhci.vistaback.user.services.AppUserService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 @Component
-@RequiredArgsConstructor
 public class PollMapper {
+  private final PollService pollService;
   private final AppUserService appUserService;
 
+  public PollMapper(@Lazy PollService pollService, AppUserService appUserService) {
+    this.pollService = pollService;
+    this.appUserService = appUserService;
+  }
+
   public PollDto toDto(Poll poll) {
-    Optional<AppUser> user = appUserService.getCurrentUser();
-    AtomicBoolean isVoted = new AtomicBoolean(false);
-    List<PollOption> options = poll.getOptions();
-
-    options.forEach(option -> {
-      if (isVoted.get() || user.isEmpty()) {
-        return;
-      }
-
-      if (appUserService.findUserByUserIdAndVotedPollOptionId(user.get().getUserId(), option.getPollOptionId()).isPresent()) {
-        isVoted.set(true);
-      }
-    });
+    boolean isVoted = pollService.isUserVoted(poll);
 
     PollDto pollDto = new PollDto(poll);
-    pollDto.setVoted(isVoted.get());
+    pollDto.setVoted(isVoted);
 
     return pollDto;
   }
 
   public Poll create(PollCreateDto pollCreateDto) {
+    Optional<AppUser> user = appUserService.getCurrentUser();
     List<PollOption> options = pollCreateDto.getOptions().stream().map(opt -> new PollOption(opt.getLabel())).toList();
+
+    if(user.isEmpty()) {
+      throw new UnauthorizedException();
+    }
 
     return new Poll(
         pollCreateDto.getTitle(),
@@ -50,6 +48,7 @@ public class PollMapper {
         pollCreateDto.getAnonymous(),
         pollCreateDto.getStartDate(),
         pollCreateDto.getEndDate(),
+        user.get(),
         options
     );
   }
