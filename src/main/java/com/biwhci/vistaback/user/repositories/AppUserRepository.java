4bdc63f1c33b955dd883a517304405d31337f813
hodame <hodame04@gmail.com>
2024-01-10 18:17:34 +0500

    remove method for finding user by poll id. add method for finding users that vote for option

--- src/main/java/com/biwhci/vistaback/user/repositories/AppUserRepository.java
+++ src/main/java/com/biwhci/vistaback/user/repositories/AppUserRepository.java
@@ -4,11 +4,12 @@ import com.biwhci.vistaback.user.models.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
-    Optional<AppUser> findByEmail(String email);
+  Optional<AppUser> findByEmail(String email);
 
-    Optional<AppUser> findUserByUserIdAndVotedPollOptionId(Integer userId, Integer pollOptionId);
+  List<AppUser> findByVotedPollOptionId(Integer pollOptionId);
 }
